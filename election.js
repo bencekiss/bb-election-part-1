d010434c83cab62c8edb46fa801f343b4e3c6af0
Bence Kiss <kiss.bence.balint@gmail.com>
2017-02-17 15:06:58 -0500

    Created the list to view candidates.

--- election.js
+++ election.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-
+  
   // Imagination!
-
+  $.ajax({
+    url: "https://bb-election-api.herokuapp.com/",
+    method: "GET",
+    dataType: "json"
+  }).done(function(responseData){
+    console.log(responseData);
+    var candidates = responseData.candidates;
+    for (var i = 0; i < candidates.length; i++) {
+      var li = $('<li>');
+      li.html(candidates[i].name + " has " + candidates[i].votes + " votes.");
+      li.appendTo('#list');
+    }
+  });
 });
